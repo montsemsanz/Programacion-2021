2ed7fa5671c92954c1a622f79da913fe4e8bf94c
montsemsanz <montsemsanz@gmail.com>
2020-10-30 07:24:45 +0100

    Añadido ejemplo Persona con Scanner e  Interfaz de Texto SOL

---
+++ UT4/Persona con Scanner e  Interfaz de Texto SOL/AppMain.java
@@ -0,0 +1,17 @@
+
+/**
+ * Clase AppMain -  incluye el método main()
+ * Arranca la aplicación
+ */
+public   class AppMain
+{
+     
+    /**
+     * 
+     */
+    public static void main(String[] args)
+    {
+         InterfazTexto interfaz = new InterfazTexto();
+         interfaz.ejecutar();
+    }
+}
