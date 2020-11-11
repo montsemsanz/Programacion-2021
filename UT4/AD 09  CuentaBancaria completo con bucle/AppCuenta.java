da55f7d9c8f309442e949dbe9efef909ea0deb9f
montsemsanz <montsemsanz@gmail.com>
2020-11-11 17:40:39 +0100

    Añadido AD09  CuentaBancaria completo con bucle

---
+++ UT4/AD 09  CuentaBancaria completo con bucle/AppCuenta.java
@@ -0,0 +1,15 @@
+
+/**
+ *  Clase con main para arrancar la aplicación
+ */
+public class AppCuenta
+{
+        public static void main (String args[])
+        {
+                InterfazTexto interfaz = new InterfazTexto();
+                interfaz.iniciar();
+        }
+        
+        
+        
+}
