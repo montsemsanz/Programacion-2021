72cfd36efb6ae20c36e0b710d6043e5adbcaa8c8
montsemsanz <montsemsanz@gmail.com>
2020-11-30 10:00:11 +0100

    Añadido Ejemplos UT5 Arrays

---
+++ UT5/Ejemplos UT5 Arrays/DemoOrdenación.java
@@ -0,0 +1,57 @@
+
+/**
+ *  
+ * 
+ * @author  
+ * @version  
+ */
+public class DemoOrdenación
+{
+
+    /**
+     * Constructor  
+     */
+    public DemoOrdenación()
+    {
+
+    }
+
+    /**
+     *  Ordenar en orden ascendente
+     */
+    public void  ordenarSeleccionDirecta(int[] array)
+    {
+        for  (int i = 0; i < array.length - 1; i++)
+        {
+            int posmin = i;
+            for (int j = i + 1; j < array.length; j++)
+            {
+                if (array[j] < array[posmin])
+                {
+                    posmin = j;
+                }
+            }
+            int aux = array[posmin];
+            array[posmin] = array[i];
+            array[i] = aux;
+        }
+    }
+
+    /**
+     *  Ordenar en orden ascendente
+     */
+    public void  ordenarInsercionDirecta(int[] array)
+    {
+        for  (int i = 1; i < array.length; i++)
+        {
+            int aux = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > aux)
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = aux;
+        }
+    }
+}
