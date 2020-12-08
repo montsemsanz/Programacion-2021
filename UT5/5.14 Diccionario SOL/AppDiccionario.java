cdc1f4c86a96ae6c351f7e2ac75d3178cabc1cd2
montsemsanz <montsemsanz@gmail.com>
2020-12-08 17:41:32 +0100

    Añadido 5.14 Diccionario SOL

---
+++ UT5/5.14 Diccionario SOL/AppDiccionario.java
@@ -0,0 +1,18 @@
+
+/**
+ *  
+ */
+public class AppDiccionario
+{
+
+    /**
+     *  
+     *  
+     */
+    public static void main(String[] args)
+    {
+        Diccionario diccionario = new Diccionario(20);
+        InterfazDiccionario interfaz =  new InterfazDiccionario(diccionario);
+        interfaz.iniciar();
+    }
+}
