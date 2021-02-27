808e966611b3e6413935c4299ce444f21e57eac6
montsemsanz <montsemsanz@gmail.com>
2021-02-27 19:47:31 +0100

    Añadido proyecto Probando otro conversor

---
+++ UT6/Probando otro conversor/AppOtroConversor.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import pkgotroconversor.romanos.ConversorRomanos;
+/**
+ * 
+ * 
+ * @author  
+ * @version  
+ */
+public class AppOtroConversor
+{
+
+    /**
+     *  
+     *  
+     */
+    public static void main(String[] args)
+    {
+        ConversorRomanos conversor = new ConversorRomanos();
+        Scanner teclado = new Scanner(System.in);
+        for (int i = 1; i <= 4; i++)   {
+            System.out.print("Dame nº arábigo: ");
+            int numero = teclado.nextInt();
+            System.out.println("Su romano es: " + conversor.aRomano(numero));
+        }
+        System.out.println("-----------------------");
+        teclado.nextLine();
+        for (int i = 1; i <= 4; i++)     {
+            System.out.print("Dame nº romano: ");
+            String romano = teclado.nextLine();
+            System.out.println("Su arábigo es: " + conversor.aArabigo(romano));
+        }
+    }
+}
