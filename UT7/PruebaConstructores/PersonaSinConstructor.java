6e22904b92cdc0d516fa1f567fb16862d090f442
montsemsanz <montsemsanz@gmail.com>
2021-03-14 19:02:37 +0100

    Añadido ejemplo prueba de constructores

---
+++ UT7/PruebaConstructores/PersonaSinConstructor.java
@@ -0,0 +1,13 @@
+
+/**
+ *  Java incluye un constructor por defecto si no se escribe uno  
+ */
+public class PersonaSinConstructor
+{
+     
+    private String nombre;
+    private int edad;
+    private double sueldo;
+
+     
+}
