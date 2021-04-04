7192c1a7cd179f5ee51aab43c2a495b2e16776db
montsemsanz <montsemsanz@gmail.com>
2021-04-04 17:36:14 +0200

    Añadido Tienda Ordenadores Abstracta

---
+++ UT7/ECL Tienda Ordenadores Abstracta/src/ordenadores/modelo/SobreMesa.java
@@ -0,0 +1,74 @@
+package ordenadores.modelo;
+
+/**
+ *    
+ */
+public class SobreMesa extends Ordenador
+{
+
+	private static final double DESCUENTO = 5;
+	private String descripcion;
+
+	/**
+	 * Constructor
+	 */
+	public SobreMesa(String codigo, double precio,
+	                String descripcion)
+	{
+
+		super(codigo, precio);
+		this.descripcion = descripcion;
+	}
+
+	/**
+	 *  
+	 * 
+	 */
+	public String getDescripcion()
+	{
+
+		return descripcion;
+	}
+
+	/**
+	 *  
+	 * 
+	 */
+	@Override
+	public String getSlogan()
+	{
+
+		return "El que más pesa pero el que menos cuesta";
+	}
+
+	/**
+	 * 
+	 */
+	public void setDescripcion(String descripcion)
+	{
+
+		this.descripcion = descripcion;
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public double calcularDescuento()
+	{
+
+		return getPrecio() * DESCUENTO / 100;
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public String toString()
+	{
+
+		return super.toString() +
+		                "Descripción: " + descripcion
+		                + "\n\n";
+	}
+}
