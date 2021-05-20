a3c9b9c46a0a3671f779eb3ea264483915a030b1
montsemsanz <montsemsanz@gmail.com>
2021-05-20 16:21:49 +0200

    Añadido de UT9 proyecto CalculadoraSimple - versión 1

---
+++ UT9/CalculadoraSimpleV1/src/pkgcalculadora/Calculadora.java
@@ -0,0 +1,26 @@
+package pkgcalculadora;
+
+public class Calculadora
+{
+	public int sumar(int n1, int n2)
+	{
+		return n1 + n2;
+	}
+
+	public int restar(int n1, int n2)
+	{
+		return n1 - n2;
+	}
+
+	public int multiplicar(int n1, int n2)
+	{
+		return n1 * n2;
+	}
+
+	public int dividir(int n1, int n2)
+	{
+
+		return n1 / n2;
+	}
+
+}
